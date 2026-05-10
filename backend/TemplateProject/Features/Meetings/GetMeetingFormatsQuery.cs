5cd5c234410b1cc7d6cb2381bae06e8a217fd9eb
clayforhome <192936966+clayforhome@users.noreply.github.com>
2026-05-10 23:23:27 +0500

    Add GetMeetingFormatsQuery endpoint to retrieve available meeting formats

---
+++ backend/TemplateProject/Features/Meetings/GetMeetingFormatsQuery.cs
@@ -0,0 +1,41 @@
+using MediatR;
+using TemplateProject.Common;
+using TemplateProject.Domain;
+
+namespace TemplateProject.Features.Meetings;
+
+public class GetMeetingFormatsQuery : IFeatureEndpoint
+{
+    public void Map(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapGet("/api/v1/meeting-formats", async (
+                IMediator mediator,
+                CancellationToken cancellationToken) =>
+            {
+                return await mediator.Send(new Request(), cancellationToken);
+            })
+            .WithName("GetMeetingFormats")
+            .WithOpenApi()
+            .WithTags("Meetings")
+            .RequireAuthorization(AuthorizationPolicy.OnlyAdminPolicy, AuthorizationPolicy.OrganizerPolicy);
+    }
+
+    public record Request : IRequest<BaseApiResponse<Response>>;
+
+    public record Response
+    {
+        public List<MeetingFormat> MeetingFormats = [];
+    }
+
+    public class Handler : IRequestHandler<Request, BaseApiResponse<Response>>
+    {
+        public async Task<BaseApiResponse<Response>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var formats = Enum.GetValues<MeetingFormat>().ToList();
+            return new Response
+            {
+                MeetingFormats = formats
+            };
+        }
+    }
+}
